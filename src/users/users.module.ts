627578e5935ca652a3350245c80e112fc31d09b8
Danil Panarin <danilpanarin@thefrontends-MacBook-Air.local>
2023-09-25 23:25:59 +0600

    initial 👈🏻

---
+++ src/users/users.module.ts
@@ -0,0 +1,12 @@
+import { Module } from '@nestjs/common';
+import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { User } from './users.model';
+import { SequelizeModule } from '@nestjs/sequelize';
+
+@Module({
+  imports: [SequelizeModule.forFeature([User])],
+  controllers: [UsersController],
+  providers: [UsersService],
+})
+export class UsersModule {}
